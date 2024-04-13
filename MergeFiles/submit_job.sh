877afcc1d61acc183a1f766928a16d3776790efc
Oliver <oliver.e.dew@gmail.com>
2024-04-13 18:19:56 +0100

    with dask

--- MergeFiles/submit_job.sh
+++ MergeFiles/submit_job.sh
@@ -3,29 +3,24 @@
 #SBATCH --job-name=Parquet_merging  # Job name
 #SBATCH --nodes=1                   # Number of nodes
 #SBATCH --ntasks-per-node=1        # Number of cores
-#SBATCH --cpus-per-task=32          # Number of threads per task
-#SBATCH --mem=80G                   # Total memory per node
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=128G                   # Total memory per node
 #SBATCH --time=01:00:00             # Time limit hrs:min:sec
 #SBATCH --output=result_%j.log      # Standard output and error log
 #SBATCH --error=error_%j.log        # Error log
 #SBATCH --mail-user=odew1@sheffield.ac.uk
 
-module load Anaconda3/2022.10
-
-# Activate the Conda environment
-source activate rapids-0.19
-
+module load Python/3.10.8-GCCcore-12.2.0
 export DATA_DIRECTORY=/mnt/parscratch/users/eia19od/Cleaned
-conda create -n rapids-env -c rapidsai -c nvidia -c conda-forge cudf=21.06 dask-cudf=21.06 python=3.8 cudatoolkit=11.0
 
 pip install dask[complete]
 pip install numpy
 pip install pandas
 pip install os
 pip install sys
-
+pip install logging
+pip install multiprocessing
 pip install fastparquet
-
 
 # start time of the job
 date  # Print the current date and time
