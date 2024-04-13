80b5a2f7c22778461172a7fdb5b3670f63048f2f
Oliver <oliver.e.dew@gmail.com>
2024-04-13 19:12:26 +0100

    with dask

--- MergeFiles/submit_job.sh
+++ MergeFiles/submit_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1                   # Number of nodes
 #SBATCH --ntasks-per-node=1        # Number of cores
 #SBATCH --cpus-per-task=32
-#SBATCH --mem=128G                   # Total memory per node
+#SBATCH --mem=64G                   # Total memory per node
 #SBATCH --time=01:00:00             # Time limit hrs:min:sec
 #SBATCH --output=result_%j.log      # Standard output and error log
 #SBATCH --error=error_%j.log        # Error log
