c05fd2d7371f3d75f578ce2d749baa294e6a7cf8
Oliver <oliver.e.dew@gmail.com>
2024-04-14 01:10:39 +0100

    openpyxl

---
+++ MergeFiles/SimplerCombine.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import os
+from multiprocessing import Pool
+
+def read_and_process_file(file):
+    df = pd.read_parquet(file)
+    if 'Part Number' not in df.columns:
+        df['Part Number'] = os.path.splitext(os.path.basename(file))[0]
+    return df
+
+def main():
+    directory = '/mnt/parscratch/users/eia19od/Cleaned'
+
+    # Get a list of all Parquet files in the directory
+    files = [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith('.parquet')]
+
+    # Use a multiprocessing Pool to read and process the files in parallel
+    with Pool() as p:
+        dataframes = p.map(read_and_process_file, files)
+
+    # Combine all DataFrames into a single DataFrame
+    df = pd.concat(dataframes, ignore_index=True)
+
+    # Save the processed DataFrame to a new parquet file
+    output_file = '/users/eia19od/in_situ/HPC-In-Situ/MergeFiles/combined.parquet'
+    df.to_parquet(output_file, index=False)
+
+if __name__ == "__main__":
+    main()
+    print("Done")
