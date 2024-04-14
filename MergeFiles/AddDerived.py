3461020fa7347aa15624ccca690bc00a93fffc7e
Oliver <oliver.e.dew@gmail.com>
2024-04-14 16:29:07 +0100

    cleaned code

---
+++ MergeFiles/AddDerived.py
@@ -0,0 +1,9 @@
+"""
+This Script is used to add derived parameters to the combined DataFrame including:
+- Normalised volumetric energy density (VED)
+- Normalised Enthalpy
+"""
+
+import os
+import pandas as pd
+import logging
--- MergeFiles/SubmitJob.sh
+++ MergeFiles/SubmitJob.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## Generic SLURM script for a serial job...
+
+
 #SBATCH --job-name=Parquet_merging  # Job name
 #SBATCH --nodes=1                   # Number of nodes
 #SBATCH --ntasks-per-node=1         # Number of cores
@@ -8,23 +11,11 @@
 #SBATCH --time=00:20:00             # Time limit hrs:min:sec
 #SBATCH --output=result.log         # Standard output and error log
 #SBATCH --error=error.log           # Error log
-#SBATCH --mail-user=odew1@sheffield.ac.uk
 
 
 source /users/eia19od/in_situ/HPC-In-Situ/venv/bin/activate
-
-
-
 date  # Print the current date and time
-
-# Run your Python script
 python /users/eia19od/in_situ/HPC-In-Situ/MergeFiles/CheckMerge.py
-
-# end time of the job
 date  # Print the current date and time
-
-# Print out resource usage
 seff $SLURM_JOBID
-
-# Deactivate the virtual environment
 deactivate
