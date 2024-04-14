851e459574087abe4c812deb1f69a03627846ce5
Oliver <oliver.e.dew@gmail.com>
2024-04-14 14:31:14 +0100

    openpyxl

--- MergeFiles/SimplerCombine_Run.sh
+++ MergeFiles/SimplerCombine_Run.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=Parquet_merging  # Job name
 #SBATCH --nodes=1                   # Number of nodes
 #SBATCH --ntasks-per-node=1         # Number of cores
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=128G                  # Total memory per node
 #SBATCH --time=00:10:00             # Time limit hrs:min:sec
 #SBATCH --output=result.log         # Standard output and error log
