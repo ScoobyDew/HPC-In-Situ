01c2464869b13a7b0abe5223ff2e84b1970e862d
Oliver <oliver.e.dew@gmail.com>
2024-04-14 13:42:16 +0100

    openpyxl

--- MergeFiles/SimplerCombine_Run.sh
+++ MergeFiles/SimplerCombine_Run.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks-per-node=1         # Number of cores
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=128G                  # Total memory per node
-#SBATCH --time=06:00:00             # Time limit hrs:min:sec
+#SBATCH --time=00:10:00             # Time limit hrs:min:sec
 #SBATCH --output=result.log         # Standard output and error log
 #SBATCH --error=error.log           # Error log
 #SBATCH --mail-user=odew1@sheffield.ac.uk
