9e2fac62a97f9f88101eac41f7fc596d761ac8d9
Oliver <oliver.e.dew@gmail.com>
2024-04-16 00:26:28 +0100

    cleaned code

--- Plots/2dHist.py
+++ Plots/2dHist.py
@@ -1,72 +1,52 @@
-"""
-Plots two of the parameteres of the data from combined_params.parquet against each other
-"""
 import logging
 import os
-import dask
-import plotly.express as px
-import pickle
+import dask.dataframe as dd
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 import time
-# use a clear background for seaborn plots
-sns.set_style("white")
-
-import dask.dataframe as dd
+import pickle
 from matplotlib.colors import LogNorm
+
+# Setup logging
+logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
+
 # Find time at the start of the processing (date and time)
 start_time = time.strftime('%Y-%m-%d %H:%M:%S')
-# Setup logging
-logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w',
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-
 
 def main():
-
-    logging.basicConfig(level=logging.INFO)
     logging.info("Starting processing")
     filepath = '/mnt/parscratch/users/eia19od/combined_params.parquet'
 
     try:
-        # Read the merged parquet file using dask_cudf
+        # Read the merged parquet file using dask
         df = dd.read_parquet(filepath, columns=['mp_width', 'mp_length'])
         logging.info(f"Successfully read parquet file: {filepath}")
 
-        import numpy as np
-
         # Replace all zero values with NaN
-        logging.info(f"Replacing all zero values with NaN...")
         df = df.replace(0, np.nan)
-        logging.info(f"Zero values replaced with NaN")
+        
+        # Check for duplicate indices
+        if df.index.duplicated().any().compute():
+            logging.error("Duplicate indices found in the DataFrame.")
+            raise ValueError("Duplicate indices found in the DataFrame.")
 
         # Convert to pandas dataframe
-        logging.info(f"Converting to pandas dataframe...")
         df_pd = df.compute()
-        logging.info(f"Converted to pandas dataframe")
+        logging.info("Converted to pandas dataframe")
 
-        logging.info(f"Plotting the data...")
-
-        # Specify vmin and vmax
-        vmin = 1
-        vmax = 7.1e6
-
-        # Create a LogNorm instance with vmin and vmax
-        norm = LogNorm(vmin=vmin, vmax=vmax)
-
-        # Plotting using Seaborn
+        # Plotting the data
         plt.figure(figsize=(10, 8))
         sns.histplot(
             data=df_pd,
             x='mp_width',
             y='mp_length',
-            cmap = 'cividis',
+            cmap='cividis',
             binwidth=(1, 1),
             cbar=True,
-            norm=LogNorm(),
+            norm=LogNorm(vmin=1, vmax=7.1e6),
         )
-        # plt.xlim(10,50)
-        # plt.ylim(10,70)
         plt.title('2D Histogram of meltpool width and length', fontsize=16)
         plt.xlabel('Melt Pool Width (pixels)')
         plt.ylabel('Melt Pool Length (pixels)')
@@ -80,19 +60,16 @@ def main():
         logging.info("Plot saved as PNG.")
 
         # Save the plot as a pickle file
-        with open(f"images/density_contour_{start_time}", 'wb') as f:
+        with open(f"images/density_contour_{start_time}.pkl", 'wb') as f:
             pickle.dump(plt.gcf(), f)
         logging.info("Plot saved as pickle.")
 
+    except Exception as e:
+        logging.error(f"Error during processing: {e}", exc_info=True)
     finally:
-        # Clear the plotting area for any further plots
-        plt.clf()
-
+        plt.clf()  # Clear the plotting area
 
     logging.info("Processing Finished")
-
-    # save the plot to a file
-
 
 if __name__ == "__main__":
     main()
