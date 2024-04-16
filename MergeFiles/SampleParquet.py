2ec7496f867afbe947bdfdc3c15350156f4320b3
Oliver <oliver.e.dew@gmail.com>
2024-04-16 20:04:34 +0100

    sampling added

--- MergeFiles/SampleParquet.py
+++ MergeFiles/SampleParquet.py
@@ -1,15 +1,8 @@
-"""
-Load the full parquet file and sample the data to make a reduced size parquet file.
-"""
-
-
 import logging
 import dask.dataframe as dd
-import os
-import time
-import numpy as np
-import gc
 import pandas as pd
+import os
+import gc
 
 # Setup logging
 logging.basicConfig(level=logging.INFO, filename='sample.log', filemode='w',
@@ -18,22 +11,31 @@ logging.basicConfig(level=logging.INFO, filename='sample.log', filemode='w',
 def sample_data(filepath, frac=0.0001):
     """Sample the data and save to a new parquet file."""
     logging.info(f"Reading parquet file: {filepath}")
-    df = dd.read_parquet(filepath)
+    df = dd.read_parquet(filepath)  # Loading with Dask
     logging.info(f"Read parquet file: {filepath}")
 
     logging.info(f"Sampling data with fraction: {frac}")
-    df_sampled = df.sample(frac=frac).compute()
+    df_sampled = df.sample(frac=frac).compute()  # Computing to convert to Pandas DataFrame
 
-    # log the columns and their data types
+    # Ensure the type after compute() is indeed a Pandas DataFrame
+    if isinstance(df_sampled, pd.DataFrame):
+        logging.info("Confirmed sampled data is a Pandas DataFrame")
+    else:
+        logging.error("Data is not a Pandas DataFrame")
+        return  # Exit if not a Pandas DataFrame
+
     logging.info(f"Columns: {df_sampled.columns}")
     logging.info(f"Data types: {df_sampled.dtypes}")
 
-
-    # Save the sampled data to a new parquet file
+    # Prepare to save the sampled data to a new parquet file
     new_filepath = '/mnt/parscratch/users/eia19od/combined_sampled.parquet'
     logging.info(f"Saving sampled data to: {new_filepath}")
-    df_sampled.to_parquet(new_filepath, engine='pyarrow', index=False)
+    df_sampled.to_parquet(new_filepath, engine='pyarrow', index=False)  # Saving with Pandas
     logging.info(f"Saved sampled data to: {new_filepath}")
+
+    # Clean up to free memory
+    del df_sampled
+    gc.collect()
 
 def main():
     filepath = '/mnt/parscratch/users/eia19od/combined_derived.parquet'
