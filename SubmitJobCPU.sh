0e649d673655228bef2f55689e9ef54413262810
Oliver <oliver.e.dew@gmail.com>
2024-04-16 01:22:36 +0100

    cleaned code

--- SubmitJobCPU.sh
+++ SubmitJobCPU.sh
@@ -16,7 +16,7 @@
 # Source the Conda initialization script
 
 source /users/eia19od/in_situ/HPC-In-Situ/venv/bin/activate
-python /users/eia19od/in_situ/HPC-In-Situ/ML/HDBSCANmeltpoolCPU.py
+python /users/eia19od/in_situ/HPC-In-Situ/Plots/2dHist.py
 date
 seff $SLURM_JOBID
 
