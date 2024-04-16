78434473a948f740501b87fb90334ae8b65188bd
Oliver <oliver.e.dew@gmail.com>
2024-04-16 01:38:46 +0100

    cleaned code

--- ML/HDBSCANmeltpoolCPU.py
+++ ML/HDBSCANmeltpoolCPU.py
@@ -6,7 +6,7 @@ import dask.dataframe as dd
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from hdbscan import HDBSCAN  # Corrected import
+from sklearn.cluster import HDBSCAN  # Corrected import
 
 # Setup logging
 logging.basicConfig(level=logging.INFO, filename='hdbscan_clustering_cpu.log', filemode='w',
