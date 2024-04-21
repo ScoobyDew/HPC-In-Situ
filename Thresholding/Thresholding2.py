1158ddae006f48199c1515dee4a7cee1c20a6c07
Oliver <oliver.e.dew@gmail.com>
2024-04-21 17:35:12 +0100

    Thresholding.py

--- Thresholding/Thresholding2.py
+++ Thresholding/Thresholding2.py
@@ -1,45 +1,47 @@
-# Load both the main dataset and the labeled dataset
-import pandas as pd
+import dask.dataframe as dd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import logging
 import time
 
 time_start = time.time()
+
 # Set up logging
-logging.basicConfig(level=logging.INFO, filename='thresh2.log', filemode='w',
+logging.basicConfig(level=logging.INFO, filename='/mnt/parscratch/users/eia19od/thresh2.log', filemode='w',
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 main_data_path = '/mnt/parscratch/users/eia19od/combined_data_with_parameters.parquet'
 labeled_data_path = '/mnt/parscratch/users/eia19od/labelled.csv'
 
-logging.info("Reading the main and labeled datasets.")
-main_data = pd.read_parquet(main_data_path)
+logging.info("Reading the main and labeled datasets using Dask.")
+main_data = dd.read_parquet(main_data_path)
 
-# Make mp_length and mp_width integer columns
+# Convert data types
 main_data['mp_length'] = main_data['mp_length'].astype(int)
 main_data['mp_width'] = main_data['mp_width'].astype(int)
 
-labeled_data = pd.read_csv(labeled_data_path)
+labeled_data = dd.read_csv(labeled_data_path)
 logging.info("Datasets read successfully.")
 
 # Merge the datasets
-# Assuming there's a common column like 'mp_length' and 'mp_width' in both datasets
 logging.info("Merging the datasets.")
-merged_data = pd.merge(main_data, labeled_data, on=['mp_length', 'mp_width'], how='left')
+merged_data = dd.merge(main_data, labeled_data, on=['mp_length', 'mp_width'], how='left')
 logging.info("Datasets merged successfully.")
 
-# Plot violin plot of 'Normalized Energy' by 'cluster' (1, 2, 3)
-import matplotlib.pyplot as plt
-import seaborn as sns
+# Compute necessary part for plotting
+logging.info("Computing necessary data for plotting.")
+computed_data = merged_data.compute()  # This triggers the actual computation
 
+# Plotting
 logging.info("Plotting the violin plot.")
 plt.figure(figsize=(12, 8))
-sns.violinplot(x='RegionLabel', y='Normalized Enthalpy', data=merged_data)
+sns.violinplot(x='RegionLabel', y='Normalized Enthalpy', data=computed_data)
 plt.title('Violin Plot of Normalized Energy by Cluster')
 plt.xlabel('Cluster')
 plt.ylabel('Normalized Energy')
 logging.info("Violin plot created successfully.")
 
-# Save the plot in \mnt\parscratch\users\eia19od
+# Save the plot
 output_path = '/mnt/parscratch/users/eia19od/violin_plot.png'
 plt.savefig(output_path)
 logging.info(f"Violin plot saved successfully at {output_path}.")
